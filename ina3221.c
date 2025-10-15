53661bbcb83b5eb1e8681be7f2160dfc4bbd4b22
Juli Gassibe <gassibe.julian@uncuyo.edu.ar>
2025-10-15 11:45:25 -0300

    Voltaje shunt y bus listas

--- ina3221.c
+++ ina3221.c
@@ -59,6 +59,94 @@ esp_err_t ina3221_get_ch1_shunt(i2c_master_dev_handle_t *ina3221Handle, int16_t 
     return ESP_OK;
 }
 
+esp_err_t ina3221_get_ch1_bus(i2c_master_dev_handle_t *ina3221Handle, int16_t *ch1Bus) {
+    esp_err_t error;
 
+    uint8_t address = CH1_BUS;
+    uint8_t rxdata[2] = {0};
+
+    error = i2c_master_transmit_receive(*ina3221Handle, &address, sizeof(address), rxdata, sizeof(rxdata), 10);
+
+    if (error != ESP_OK) {
+        ESP_LOGE(tag, "Error al leer voltaje del bus CH1: %s", esp_err_to_name(error));
+        return error;
+    }
+
+    *ch1Bus = ((rxdata[0] << 8) | rxdata[1]) >> 3;
+
+    return ESP_OK;
+}
+
+esp_err_t ina3221_get_ch2_shunt(i2c_master_dev_handle_t *ina3221Handle, int16_t *ch2Shunt) {
+    esp_err_t error;
+
+    uint8_t address = CH2_SHUNT;
+    uint8_t rxdata[2] = {0};
+
+    error = i2c_master_transmit_receive(*ina3221Handle, &address, sizeof(address), rxdata, sizeof(rxdata), 10);
+
+    if (error != ESP_OK) {
+        ESP_LOGE(tag, "Error al leer voltaje shunt CH2: %s", esp_err_to_name(error));
+        return error;
+    }
+
+    *ch2Shunt = ((rxdata[0] << 8) | rxdata[1]) >> 3;
+
+    return ESP_OK;
+}
+
+esp_err_t ina3221_get_ch2_bus(i2c_master_dev_handle_t *ina3221Handle, int16_t *ch2Bus) {
+    esp_err_t error;
+
+    uint8_t address = CH2_BUS;
+    uint8_t rxdata[2] = {0};
+
+    error = i2c_master_transmit_receive(*ina3221Handle, &address, sizeof(address), rxdata, sizeof(rxdata), 10);
+
+    if (error != ESP_OK) {
+        ESP_LOGE(tag, "Error al leer voltaje del bus CH2: %s", esp_err_to_name(error));
+        return error;
+    }
+
+    *ch2Bus = ((rxdata[0] << 8) | rxdata[1]) >> 3;
+
+    return ESP_OK;
+}
+
+esp_err_t ina3221_get_ch3_shunt(i2c_master_dev_handle_t *ina3221Handle, int16_t *ch3Shunt) {
+    esp_err_t error;
+
+    uint8_t address = CH3_SHUNT;
+    uint8_t rxdata[2] = {0};
+
+    error = i2c_master_transmit_receive(*ina3221Handle, &address, sizeof(address), rxdata, sizeof(rxdata), 10);
+
+    if (error != ESP_OK) {
+        ESP_LOGE(tag, "Error al leer voltaje shunt CH3: %s", esp_err_to_name(error));
+        return error;
+    }
+
+    *ch3Shunt = ((rxdata[0] << 8) | rxdata[1]) >> 3;
+
+    return ESP_OK;
+}
+
+esp_err_t ina3221_get_ch3_bus(i2c_master_dev_handle_t *ina3221Handle, int16_t *ch3Bus) {
+    esp_err_t error;
+
+    uint8_t address = CH3_BUS;
+    uint8_t rxdata[2] = {0};
+
+    error = i2c_master_transmit_receive(*ina3221Handle, &address, sizeof(address), rxdata, sizeof(rxdata), 10);
+
+    if (error != ESP_OK) {
+        ESP_LOGE(tag, "Error al leer voltaje del bus CH3: %s", esp_err_to_name(error));
+        return error;
+    }
+
+    *ch3Bus = ((rxdata[0] << 8) | rxdata[1]) >> 3;
+
+    return ESP_OK;
+}
 
 

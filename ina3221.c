2bddfc2e1a939a160c13a51f047e499a1e8895a2
Juli Gassibe <gassibe.julian@uncuyo.edu.ar>
2025-10-14 23:58:32 -0300

    Mas funciones nuevas

--- ina3221.c
+++ ina3221.c
@@ -5,11 +5,11 @@ static const char *tag = "ina3221.c";
 esp_err_t ina3221_get_config(i2c_master_dev_handle_t *ina3221Handle, uint16_t *config) {
     esp_err_t error;
 
-    uint8_t address = CONFIG;
+    uint8_t address = INA_CONFIG;
     uint8_t rxdata[2] = {0};
 
     // Primero debo escribir direccion del registro a leer y el sensor responde con el valor de ese registro
-    error = i2c_master_transmit_receive(*ina3221Handle, &address, sizeof(address), rxdata, 2, -1);
+    error = i2c_master_transmit_receive(*ina3221Handle, &address, sizeof(address), rxdata, 2, 10);
 
     if (error != ESP_OK) {
         ESP_LOGE(tag, "Error al leer configuración: %s", esp_err_to_name(error));
@@ -21,3 +21,44 @@ esp_err_t ina3221_get_config(i2c_master_dev_handle_t *ina3221Handle, uint16_t *c
 
     return ESP_OK;
 }
+
+esp_err_t ina3221_set_config(i2c_master_dev_handle_t *ina3221Handle, uint16_t config) {
+    esp_err_t error;
+
+    // Trama: SLAVE_ADDR - REG_ADDR - MSB - LSB
+    // En este buffer va: REG_ADDR - MSB - LSB
+    uint8_t txdata[3] = {INA_CONFIG, (uint8_t)(config >> 8), (uint8_t)(config & 0xFF)};
+
+    error = i2c_master_transmit(*ina3221Handle, txdata, sizeof(txdata), 10);
+
+    if (error != ESP_OK) {
+        ESP_LOGE(tag, "Error al escribir configuración: %s", esp_err_to_name(error));
+        return error;
+    }
+
+    printf("Configuracion grabada: %d\n", config);
+
+    return ESP_OK;
+}
+
+esp_err_t ina3221_get_ch1_shunt(i2c_master_dev_handle_t *ina3221Handle, int16_t *ch1Shunt) {
+    esp_err_t error;
+
+    uint8_t address = CH1_SHUNT;
+    uint8_t rxdata[2] = {0};
+
+    error = i2c_master_transmit_receive(*ina3221Handle, &address, sizeof(address), rxdata, sizeof(rxdata), 10);
+
+    if (error != ESP_OK) {
+        ESP_LOGE(tag, "Error al leer voltaje shunt CH1: %s", esp_err_to_name(error));
+        return error;
+    }
+
+    *ch1Shunt = ((rxdata[0] << 8) | rxdata[1]) >> 3;
+
+    return ESP_OK;
+}
+
+
+
+
--- ina3221.h
+++ ina3221.h
@@ -6,12 +6,12 @@
 #include "esp_log.h"
 
 typedef enum INA3221_ADDR_t {
-    CONFIG            = 0x00,
-    CH1_SH            = 0x01,
+    INA_CONFIG        = 0x00,
+    CH1_SHUNT         = 0x01,
     CH1_BUS           = 0x02,
-    CH2_SH            = 0x03,
+    CH2_SHUNT         = 0x03,
     CH2_BUS           = 0x04,
-    CH3_SH            = 0x05,
+    CH3_SHUNT         = 0x05,
     CH3_BUS           = 0x06,
     CH1_CAL           = 0x07,
     CH1_WAL           = 0x08,
@@ -28,7 +28,52 @@ typedef enum INA3221_ADDR_t {
     DIE_ID            = 0xFF
 };
 
+// Se deberá hacer operacion OR entre todas las configs que se deseen
+typedef enum INA3221_CONFIG_t {
+    INA_RST = 0x8000,
+    CH1_EN = 0x4000,
+    CH2_EN = 0x2000,
+    CH3_EN = 0x1000,
+    AVG_1_SAMPLE = 0x0000,
+    AVG_4_SAMPLE = 0x0200,
+    AVG_16_SAMPLE = 0x0400,
+    AVG_64_SAMPLE = 0x0600,
+    AVG_128_SAMPLE = 0x0800,
+    AVG_256_SAMPLE = 0x0A00,
+    AVG_512_SAMPLE = 0x0C00,
+    AVG_1024_SAMPLE = 0x0E00,
+    BUS_CT_140US = 0x0000,
+    BUS_CT_204US = 0x0040,
+    BUS_CT_332US = 0x0080,
+    BUS_CT_588US = 0x00C0,
+    BUS_CT_1MS = 0x0100,
+    BUS_CT_2MS = 0x0140,
+    BUS_CT_4MS = 0x0180,
+    BUS_CT_8MS = 0x01C0,
+    SHUNT_CT_140MS = 0x0000,
+    SHUNT_CT_204US = 0x0008,
+    SHUNT_CT_332US = 0x0010,
+    SHUNT_CT_588US = 0x0018,
+    SHUNT_CT_1MS = 0x0020,
+    SHUNT_CT_2MS = 0x0028,
+    SHUNT_CT_4MS = 0x0030,
+    SHUNT_CT_8MS = 0x0038,
+    MODE_PD = 0x0000,
+    MODE_SHUNT_SS = 0x0001,
+    MODE_BUS_SS = 0x0002,
+    MODE_SHUNT_CONT = 0x0005,
+    MODE_BUS_CONT = 0x0006
+};
+
 esp_err_t ina3221_get_config(i2c_master_dev_handle_t *ina3221Handle, uint16_t *config);
+esp_err_t ina3221_set_config(i2c_master_dev_handle_t *ina3221Handle, uint16_t config);
+esp_err_t ina3221_get_ch1_shunt(i2c_master_dev_handle_t *ina3221Handle, int16_t *ch1Shunt);
+esp_err_t ina3221_get_ch1_bus(i2c_master_dev_handle_t *ina3221Handle, int16_t *ch1Bus);
+esp_err_t ina3221_get_ch2_shunt(i2c_master_dev_handle_t *ina3221Handle, int16_t *ch2Shunt);
+esp_err_t ina3221_get_ch2_bus(i2c_master_dev_handle_t *ina3221Handle, int16_t *ch2Bus);
+esp_err_t ina3221_get_ch3_shunt(i2c_master_dev_handle_t *ina3221Handle, int16_t *ch3Shunt);
+esp_err_t ina3221_get_ch3_bus(i2c_master_dev_handle_t *ina3221Handle, int16_t *ch3Bus);
+
 
 
 
